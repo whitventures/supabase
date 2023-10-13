dea3c137ac4c8ec9e8852f4768196fa67ff575a6
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-10-13 15:20:58 +0200

    Update Reports.constants.ts

--- studio/components/interfaces/Reports/Reports.constants.ts
+++ studio/components/interfaces/Reports/Reports.constants.ts
@@ -381,8 +381,9 @@ select
       },
       databaseSpace: {
         queryType: 'db',
-        sql: (_) => `select sum(pg_database_size(pg_database.datname))::bigint as db_size from pg_database;`
-      }
+        sql: (_) =>
+          `select sum(pg_database_size(pg_database.datname))::bigint as db_size from pg_database;`,
+      },
     },
   },
 }
